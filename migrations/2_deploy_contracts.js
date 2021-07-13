74133f49806f8f3c9deed0b4e6c004d49c688935
alisidaniel <alisidaniel@gmail.com>
2021-07-13 23:35:54 +0100

    deployed new contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function (deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function (deployer, networks, accounts) {
+  //Deploy Mock DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  //Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  //deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Transfer all tokens to tokensFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
+
+  // Transfer 100 Mocks DAI tokens to investor
+  await daiToken.transfer(accounts[1], "100000000000000000000");
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import './DappToken.sol';
+import './DaiToken.sol';
+
 contract TokenFarm{
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
