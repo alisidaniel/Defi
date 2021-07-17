af7e26c6a996c59518aa96ba1c8c4a540d5dd31e
alisidaniel <alisidaniel@gmail.com>
2021-07-17 01:19:21 +0100

    ui update

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS
+          <span className="font-semibold text-xl text-black tracking-tight">
+            Dapp
           </span>
         </div>
         <div className="block lg:hidden">
@@ -37,19 +37,19 @@ function App() {
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
-              Docs
+              Tab 1
             </a>
             <a
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
-              Examples
+              Tab 2
             </a>
             <a
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
             >
-              Blog
+              Tab 3
             </a>
           </div>
           <div>
@@ -64,23 +64,39 @@ function App() {
       </nav>
       <section className="flex justify-center">
         <form className="w-full max-w-lg">
-          <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="flex flex-row justify-between w-full px-3">
+            <div class="text-gray-900 text-center px-4 py-2 m-2">
+              <p>Staking Balance</p>
+              <p>0 mDAI</p>
+            </div>
+            <div class="text-gray-900 text-center px-4 py-2 m-2">
+              <p>Reward Balance</p>
+              <p>0 DAPP</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6 border pt-3 pb-4">
+            <div className="flex flex-row justify-between w-full px-3">
+              <div class="text-gray-900 text-center px-4 py-2 m-2">
+                Stake Tokens
+              </div>
+              <div class="text-gray-700 text-center px-4 py-2 m-2">
+                Balance: 100
+              </div>
+            </div>
             <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
-              >
-                Password
-              </label>
               <input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
                 type="number"
-                value="0"
+                placeholder="0"
               />
-              <p className="text-gray-600 text-xs italic">
-                Make it as long and as crazy as you'd like
-              </p>
+            </div>
+            <div class="w-full px-3">
+              <button
+                class="shadow bg-purple-500 w-full hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+              >
+                Stake
+              </button>
             </div>
           </div>
         </form>
